e98b2c17e704a60a5445d84197b0dcd2290b65c5
Aliya <aliyamohammadi043@email.com>
2025-12-01 23:37:03 +0530

    updated

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,18 @@
-const express=require("express");
-const pool=require("./db");
+const express = require("express");
+const pool = require("./db");
 require("dotenv").config();
 
-const app=express();
+const app = express();
 app.use(express.json());
 
+app.get("/tasks", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM tasks ORDER BY id ASC");
+    res.json(result.rows);
+  } catch (error) {
+    console.error("Error fetching data", error.message);
+    res.status(500).json({ error: error.message });
+  }
+});
 
-module.exports=app;
+module.exports = app;
