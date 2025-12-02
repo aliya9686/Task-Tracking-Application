498afafdb0889dfa9067ff50226b243d7a5c1379
Aliya <aliyamohammadi043@email.com>
2025-12-02 22:49:37 +0530

    Creadting and managing redux

---
+++ front/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="p-10">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Tailwind is working 🎉
+      </h1>
+
+      <div className="w-20 h-20 bg-red-500 mt-4"></div>
+    </div>
+  );
+}
+
+export default App;
---
+++ front/src/features/tasks/taskSlice.js
@@ -0,0 +1,45 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { fetchTasks, addTask, updateTask, deleteTask } from "./taskOperations";
+
+const taskSlice = createSlice({
+  name: "tasks",
+  initialState: {
+    list: [],
+    loading: false,
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      //Using cases to fetch tasks
+      .addCase(fetchTasks.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(fetchTasks.fulfilled, (state, action) => {
+        state.loading = false;
+        state.list = action.payload;
+      })
+      .addCase(fetchTasks.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      })
+
+      // to Add Task
+      .addCase(addTask.fulfilled, (state, action) => {
+        state.list.push(action.payload);
+      })
+
+      // to  Update Task
+      .addCase(updateTask.fulfilled, (state, action) => {
+        const index = state.list.findIndex(t => t.id === action.payload.id);
+        if (index !== -1) state.list[index] = action.payload;
+      })
+
+      // to Delete Task
+      .addCase(deleteTask.fulfilled, (state, action) => {
+        state.list = state.list.filter(task => task.id !== action.payload);
+      });
+  },
+});
+
+export default taskSlice.reducer;
